a02907457f822f0f655c7f2cc9579ad484740ce1
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-09-02 15:33:12 +0800

    路由处理

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import dva from 'dva';
 import './index.css';
+import { createBrowserHistory as createHistory } from 'history';
 
-// 1. Initialize
-const app = dva();
+//初始化
+const app = dva({
+  history: createHistory()
+
+});
 
 // 2. Plugins
 // app.use({});
